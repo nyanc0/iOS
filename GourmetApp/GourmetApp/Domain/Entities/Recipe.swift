fc1c3f3e5e8044a31552d452f8f543844e1b6061
yurina fukuoka <r0lling.beat193@gmail.com>
2019-03-21 23:25:04 +0900

    add url

--- GourmetApp/GourmetApp/Domain/Entities/Recipe.swift
+++ GourmetApp/GourmetApp/Domain/Entities/Recipe.swift
@@ -4,7 +4,7 @@
 //
 //  Created by yurina fukuoka on 2019/03/20.
 //  Copyright © 2019年 yurina fukuoka. All rights reserved.
-//
+// https://qiita.com/Kuniwak/items/a972ff2ade643799d1fe#インターフェースを想像する
 
 import Foundation
 struct Recipe {
