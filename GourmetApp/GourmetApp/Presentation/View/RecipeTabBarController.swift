674820f56a168160f2245ef494492522252f3d42
福岡 侑里奈 <y_fukuoka@waku-2.com>
2019-04-01 14:05:44 +0900

    アイコンの追加と画面の更新

---
+++ GourmetApp/GourmetApp/Presentation/View/RecipeTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeTabBarController.swift
+//  GourmetApp
+//
+//  Created by 福岡　侑里奈 on 2019/04/01.
+//  Copyright © 2019 yurina fukuoka. All rights reserved.
+//
+
+import UIKit
+class RecipeTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+    }
+}
