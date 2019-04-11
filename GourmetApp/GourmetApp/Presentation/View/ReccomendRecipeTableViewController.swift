e6994e2b317ad1ba208ff36c9dc7a93fdbc3d62c
福岡 侑里奈 <y_fukuoka@waku-2.com>
2019-04-11 09:55:43 +0900

    レイアウト調整

--- GourmetApp/GourmetApp/Presentation/View/ReccomendRecipeTableViewController.swift
+++ GourmetApp/GourmetApp/Presentation/View/ReccomendRecipeTableViewController.swift
@@ -11,50 +11,44 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 class ReccomendRecipeTableViewController: UIViewController, UITableViewDelegate {
-
+    
     let disposeBag: DisposeBag = DisposeBag()
     var viewModel: ReccomendListViewModel!
-    //    let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Recipe>>(
-    //        configureCell: { (_, tableView, _, recipes) in
-    //            let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell") as! RecipeTableViewCell
-    //            cell.setData(recipe: recipes)
-    //            return cell
-    //    }
-    //    )
-
+    
     @IBOutlet weak var reccomendTableView: UITableView!
     @IBOutlet weak var tableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
         initViewModel()
         bindViewModel()
     }
-
+    
     private func setUpTableView() {
         tableView.rx.setDelegate(self).disposed(by: disposeBag)
-        tableView.register(UINib(nibName: "RecipeTableViewCell", bundle: nil), forCellReuseIdentifier: "RecipeTableViewCell")
-        //        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        tableView.rowHeight = 300
+        tableView.register(UINib(nibName: "RecipeCell", bundle: nil), forCellReuseIdentifier: "RecipeCell")
     }
-
+    
     private func initViewModel() {
         viewModel = ReccomendListViewModel(recipeListUseCase: RecipeListUseCase(recipeListRepository: RecipeListRepositoryImpl()))
     }
-
+    
     private func bindViewModel() {
         let input = ReccomendListViewModel.Input(
             trigger: Driver.just(()),
             tapCell: tableView.rx.itemSelected.asSignal().map { $0.row})
-
+        
         let output = viewModel.transform(input: input)
-
-        output.recipeList.asObservable().bind(to: tableView.rx.items(cellIdentifier: "RecipeTableViewCell", cellType: RecipeTableViewCell.self)) {  (_, element, cell) in
+        
+        output.recipeList.asObservable().bind(to: tableView.rx.items(cellIdentifier: "RecipeCell", cellType: RecipeCell.self)) {  (_, element, cell) in
             //            cell.textLabel?.text = element.introduction
             cell.setData(recipe: element)
+            cell.shadowAndBorderForCell()
             }.disposed(by: disposeBag)
-
-
+        
+        
         output.load.drive().disposed(by: disposeBag)
         // TODO:インジケーター
     }
--- GourmetApp/GourmetApp/Presentation/View/RecipeCell.swift
+++ GourmetApp/GourmetApp/Presentation/View/RecipeCell.swift
@@ -9,25 +9,40 @@
 import UIKit
 import SDWebImage
 class RecipeCell: UITableViewCell {
-
+    
     @IBOutlet weak var recipeImage: UIImageView!
     @IBOutlet weak var introduction: UILabel!
-
-
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
     func setData(recipe: Recipe) {
         recipeImage.sd_setImage(with: URL(string: recipe.mainUrl), placeholderImage: UIImage(named: "ic_no_image.png"))
         introduction.text = recipe.introduction
     }
+    
+}
 
+extension UITableViewCell {
+    func shadowAndBorderForCell() {
+        self.contentView.layer.cornerRadius = 5
+        self.contentView.layer.borderWidth = 0.5
+        self.contentView.layer.borderColor = UIColor.lightGray.cgColor
+        self.contentView.layer.masksToBounds = true
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
+    }
 }
