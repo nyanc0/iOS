4431545c2b152f5d9006f0fffb648e8c3fae0738
福岡 侑里奈 <y_fukuoka@waku-2.com>
2019-04-15 14:26:26 +0900

    インジケーター処理を追加

---
+++ GourmetApp/GourmetApp/Presentation/View/FavoriteTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FavoriteTableViewController.swift
+//  GourmetApp
+//
+//  Created by 福岡　侑里奈 on 2019/04/15.
+//  Copyright © 2019 yurina fukuoka. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+class FavoriteTableViewController: UIViewController, UITableViewDelegate {
+    
+    let disposeBag: DisposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
